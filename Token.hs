51e1ce04465c590d7aa4edd5f318622cc383ed31
Bart Massey <bart@cs.pdx.edu>
2013-06-26 01:28:20 -0700

    added license

--- Token.hs
+++ Token.hs
@@ -1,4 +1,7 @@
--- Copyright © 2013 Bart Massey
+--- Copyright © 2013 Bart Massey
+--- [This program is licensed under the "MIT License"]
+--- Please see the file COPYING in the source
+--- distribution of this software for license terms.
 
 module Token (Token(..), Incrementable(..), comparable) where
 
