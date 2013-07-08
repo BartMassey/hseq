4ef5f8bf6670a2a637fef1c7e55a65d25ff0a213
Bart Massey <bart@cs.pdx.edu>
2013-07-08 10:56:49 -0700

    converted Roman to uppercase; haven't fixed infrastructure yet

--- Roman.hs
+++ Roman.hs
@@ -16,38 +16,39 @@ instance Read Roman where
 
 toRoman :: Int -> String
 toRoman n
-  | n < 4 = replicate n 'i'
-  | n == 4 = "iv"
-  | n < 9 = "v" ++ toRoman (n - 5)
-  | n == 9 = "ix"
+  | n <= 0 = error "only positive integers are representable as roman numerals"
+  | n < 4 = replicate n 'I'
+  | n < 5 = "IV"
+  | n < 9 = "V" ++ toRoman (n - 5)
+  | n < 10 = "IX"
   | n < 40 = let d = n `div` 10 in
-             replicate d 'x' ++ toRoman (n - 10 * d)
-  | n < 50 = "xl" ++ toRoman (n - 40)
+             replicate d 'X' ++ toRoman (n - 10 * d)
+  | n < 50 = "XL" ++ toRoman (n - 40)
   | n < 90 = let d = (n - 50) `div` 10 in
-             "l" ++ replicate d 'x' ++ toRoman (n - 50 - 10 * d)
-  | n < 100 = "xc" ++ toRoman (n - 90)
+             "L" ++ replicate d 'X' ++ toRoman (n - 50 - 10 * d)
+  | n < 100 = "XC" ++ toRoman (n - 90)
   | n < 400 = let d = n `div` 100 in
-             replicate d 'c' ++ toRoman (n - 100 * d)
-  | n < 500 = "cd" ++ toRoman (n - 400)
+             replicate d 'C' ++ toRoman (n - 100 * d)
+  | n < 500 = "CD" ++ toRoman (n - 400)
   | n < 900 = let d = (n - 500) `div` 100 in
-             "d" ++ replicate d 'c' ++ toRoman (n - 500 - 100 * d)
-  | n < 1000 = "cm" ++ toRoman (n - 900)
+             "D" ++ replicate d 'C' ++ toRoman (n - 500 - 100 * d)
+  | n < 1000 = "CM" ++ toRoman (n - 900)
   | otherwise = let d = n `div` 1000 in
-                replicate d 'm' ++ toRoman (n - 1000 * d)
+                replicate d 'M' ++ toRoman (n - 1000 * d)
 
 fromRoman :: String -> Int
-fromRoman ('m' : ds) = 1000 + fromRoman ds
-fromRoman ('d' : ds) = 500 + fromRoman ds
-fromRoman ('c' : 'd' : ds) = 400 + fromRoman ds
-fromRoman ('c' : 'm' : ds) = 900 + fromRoman ds
-fromRoman ('c' : ds) = 100 + fromRoman ds
-fromRoman ('l' : ds) = 50 + fromRoman ds
-fromRoman ('x' : 'l' : ds) = 40 + fromRoman ds
-fromRoman ('x' : 'c' : ds) = 90 + fromRoman ds
-fromRoman ('x' : ds) = 10 + fromRoman ds
-fromRoman "iv" = 4
-fromRoman "ix" = 9
-fromRoman ('v' : ds) = 5 + fromRoman ds
-fromRoman ('i' : ds) = 1 + fromRoman ds
+fromRoman ('M' : ds) = 1000 + fromRoman ds
+fromRoman ('D' : ds) = 500 + fromRoman ds
+fromRoman ('C' : 'D' : ds) = 400 + fromRoman ds
+fromRoman ('C' : 'M' : ds) = 900 + fromRoman ds
+fromRoman ('C' : ds) = 100 + fromRoman ds
+fromRoman ('L' : ds) = 50 + fromRoman ds
+fromRoman ('X' : 'L' : ds) = 40 + fromRoman ds
+fromRoman ('X' : 'C' : ds) = 90 + fromRoman ds
+fromRoman ('X' : ds) = 10 + fromRoman ds
+fromRoman "IV" = 4
+fromRoman "IX" = 9
+fromRoman ('V' : ds) = 5 + fromRoman ds
+fromRoman ('I' : ds) = 1 + fromRoman ds
 fromRoman "" = 0
 fromRoman (d : _) = error $ "illegal digit " ++ [d] ++ " in roman numeral"
