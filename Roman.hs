edc892c8565c3ed91eefc53587bd4586b896923c
Bart Massey <bart@cs.pdx.edu>
2013-07-08 11:08:30 -0700

    added (painful) check for negative in toRoman

--- Roman.hs
+++ Roman.hs
@@ -15,26 +15,33 @@ instance Read Roman where
   readsPrec _ s = [(Roman (fromRoman s), "")]
 
 toRoman :: Int -> String
-toRoman n
-  | n <= 0 = error "only positive integers are representable as roman numerals"
-  | n < 4 = replicate n 'I'
-  | n < 5 = "IV"
-  | n < 9 = "V" ++ toRoman (n - 5)
-  | n < 10 = "IX"
-  | n < 40 = let d = n `div` 10 in
-             replicate d 'X' ++ toRoman (n - 10 * d)
-  | n < 50 = "XL" ++ toRoman (n - 40)
-  | n < 90 = let d = (n - 50) `div` 10 in
-             "L" ++ replicate d 'X' ++ toRoman (n - 50 - 10 * d)
-  | n < 100 = "XC" ++ toRoman (n - 90)
-  | n < 400 = let d = n `div` 100 in
-             replicate d 'C' ++ toRoman (n - 100 * d)
-  | n < 500 = "CD" ++ toRoman (n - 400)
-  | n < 900 = let d = (n - 500) `div` 100 in
-             "D" ++ replicate d 'C' ++ toRoman (n - 500 - 100 * d)
-  | n < 1000 = "CM" ++ toRoman (n - 900)
-  | otherwise = let d = n `div` 1000 in
-                replicate d 'M' ++ toRoman (n - 1000 * d)
+toRoman n0
+  | n0 <= 0 = 
+      error "only positive integers are representable as roman numerals"
+  | otherwise = 
+      tr n0
+      where
+        tr n
+          | n < 0 = "internal error: negative roman numeral"
+          | n < 1 = ""
+          | n < 4 = replicate n 'I'
+          | n < 5 = "IV"
+          | n < 9 = "V" ++ replicate (n - 5) 'I'
+          | n < 10 = "IX"
+          | n < 40 = let d = n `div` 10 in
+                     replicate d 'X' ++ tr (n - 10 * d)
+          | n < 50 = "XL" ++ tr (n - 40)
+          | n < 90 = let d = (n - 50) `div` 10 in
+                     "L" ++ replicate d 'X' ++ tr (n - 50 - 10 * d)
+          | n < 100 = "XC" ++ tr (n - 90)
+          | n < 400 = let d = n `div` 100 in
+                     replicate d 'C' ++ tr (n - 100 * d)
+          | n < 500 = "CD" ++ tr (n - 400)
+          | n < 900 = let d = (n - 500) `div` 100 in
+                     "D" ++ replicate d 'C' ++ tr (n - 500 - 100 * d)
+          | n < 1000 = "CM" ++ tr (n - 900)
+          | otherwise = let d = n `div` 1000 in
+                        replicate d 'M' ++ tr (n - 1000 * d)
 
 fromRoman :: String -> Int
 fromRoman ('M' : ds) = 1000 + fromRoman ds
